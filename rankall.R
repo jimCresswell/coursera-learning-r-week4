3b7974ae36f3a471cc9cd46dc8f1cb87367ba133
jimCresswell <jim.cresswell@gmail.com>
2016-09-06 09:26:09 +0100

    Working function with for loop.

--- rankall.R
+++ rankall.R
@@ -5,26 +5,26 @@ rankall <- function(outcome, num = "best") {
                "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
   
   ## Read outcome data
-  hospital.data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  hospital.data <- read.csv("outcome-of-care-measures.csv", na.strings="Not Available", stringsAsFactors=FALSE)
   
   ## Check that outcome is valid
   if (! outcome %in% names(outcomes)) stop("invalid outcome")
   
+  # Get alphabetical list of states
+  states <- unique(hospital.data$State)
+  states <- states[order(states)]
   
   ## Create the output data frame.
-  hospitals <- data.frame(hospital = rep(NA, length(state.abb)),
-                             state = state.abb,
-                             row.names = state.abb)
+  hospitals <- data.frame(hospital = rep(NA, length(states)),
+                          state = states,
+                          row.names = states)
   
   ## For each state, find the hospital of the given rank
-  lapply(split(hospital.data, hospital.data$State), function(hospital.by.state) {
-    state <- hospital.by.state$State
+  for (state in states) {
 
     # Get the rates for that outcome, include Hospital.Name
-    rates <- hospital.by.state[, c("Hospital.Name", outcomes[outcome])]
-    
-    # Convert outcomes from character to numeric.
-    rates[, 2] <- as.numeric(rates[, 2])
+    # Get the rates for that state and outcome, include Hospital.Name
+    rates <- hospital.data[hospital.data$State == state, c("Hospital.Name", outcomes[outcome])]
     
     # Remove hospitals with undefined outcomes.
     rates <- rates[complete.cases(rates),]
@@ -32,21 +32,16 @@ rankall <- function(outcome, num = "best") {
     # Order the hospitals by rates then names.
     rates <- rates[order(rates[,2], rates[,1]),]
     
-
     # Determine the hospital rank desired and rank validity.
     num.hospitals <- nrow(rates)
-    if (num == "best") num <- 1
-    if (num == "worst") num <- num.hospitals
-    if (num > num.hospitals) return(NA)
+    if (num == "best") hospital.rank <- 1
+    if (num == "worst") hospital.rank <- num.hospitals
+    if (hospital.rank > num.hospitals) next()
     
-    rates[num, "Hospital.name"]
-    
-    # Set the hospital of the appropriate rank.
-    #hospitals[state]$hospital <- rates[num, "Hospital.name"]
-    
-  })
- 
-   ## Return a data frame with the hospital names and the
+    hospitals[state,]$hospital <- rates[hospital.rank,]$Hospital.Name
+  }
+  
+  ## Return a data frame with the hospital names and the
   ## (abbreviated) state name
-  #hospitals
+  hospitals
 }
